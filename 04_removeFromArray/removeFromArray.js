2b21b0822f6e3393008d6b0609ca500101989424
iavalen1 <iavalen1@Outlook.com>
2022-05-11 19:23:34 -0700

    Complete javascript exercises 4 and 5

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(...args) {
+  const array = args[0];
+  const result = [];
 
+  array.forEach(element => {
+    if(!args.includes(element)) {
+      result.push(element);
+    }
+  });
+
+  return result;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+  if (typeof num1 != "number" || typeof num2 != "number") {return "ERROR";}
+  else if (num1 < 0 || num2 < 0) {return "ERROR";}
+  
+  let result = 0;
+  let low = Math.min(num1,num2);
+  let high = Math.max(num1,num2);
 
+  for (let i = low; low <= high; low++) {
+    result += low;
+  }
+
+  return result;
 };
 
 // Do not edit below this line
